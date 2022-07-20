0cc3c720faa1c69b5949d5a9b1ffa2a2f8294c2e
Sinebi Innazo <sinebi.innazo@gmail.com>
2022-07-20 22:11:33 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    const header={
+        name:"Expense"
+    }
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+    </div>
+  )
+}
+
+export default Header
